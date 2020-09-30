470cb59a328afe4dcd80101e8c7776d9e4b3749f
Keel <keel.hsu@gmail.com>
2020-09-30 17:28:39 +0800

    格式化

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,85 @@
-const { resolve } = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   output: {
-    filename: 'js/built.js',
-    path: resolve(__dirname, 'dist')
+    filename: "js/built.js",
+    path: resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.js|jsx$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
         options: {
           // 预设：指示babel做怎么样的兼容性处理
           presets: [
-            '@babel/preset-react',
+            "@babel/preset-react",
             [
-              '@babel/preset-env',
+              "@babel/preset-env",
               {
                 // 按需加载
-                useBuiltIns: 'usage',
+                useBuiltIns: "usage",
                 // 指定core-js版本
                 corejs: {
-                  version: 3
+                  version: 3,
                 },
                 // 指定兼容性做到哪个版本浏览器
                 targets: {
-                  chrome: '60',
-                  firefox: '60',
-                  ie: '9',
-                  safari: '10',
-                  edge: '17'
-                }
-              }
-            ]
+                  chrome: "60",
+                  firefox: "60",
+                  ie: "9",
+                  safari: "10",
+                  edge: "17",
+                },
+              },
+            ],
           ],
-          plugins:[['import', {libraryName: 'antd', style: 'css'}]]
-        }
-      },{
+          plugins: [["import", { libraryName: "antd", style: "css" }]],
+        },
+      },
+      {
         test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }, {
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }, {
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      }, {
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
         test: /\.html$/,
-        loader: 'html-loader'
-      }, {
+        loader: "html-loader",
+      },
+      {
         test: /\.(jpg|png|gif)$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 8 * 1024,
           esModule: false,
-          name: '[hash:10].[ext]'
-        }
-      }
-    ]
+          name: "[hash:10].[ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
+      template: "./public/index.html",
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.json']
+    extensions: [".js", ".jsx", ".json"],
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
-    contentBase: resolve(__dirname, 'dist'),
+    contentBase: resolve(__dirname, "dist"),
     port: 18001,
-    open: true
-  }
-}
+    open: true,
+  },
+};
