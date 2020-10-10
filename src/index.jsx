055e6df56b06151a0f92af8b08d8b3652c729a3c
Keel <keel.hsu@gmail.com>
2020-10-10 10:37:52 +0800

    引入recoil

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import LifeNote from './components/login';
+import { RecoilRoot } from 'recoil';
+import LifeNote from './components/home';
 
 ReactDOM.render(
   // <React.StrictMode>
+  <RecoilRoot>
     <LifeNote />
+  </RecoilRoot>
   // </React.StrictMode>
   ,
   document.getElementById('root')
