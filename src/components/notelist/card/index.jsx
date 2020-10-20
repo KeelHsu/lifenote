8072a33a525cb4b6b123b6c12c0bd96ecfe4e333
Keel <keel.hsu@gmail.com>
2020-10-20 08:49:05 +0800

    解构

--- src/components/notelist/card/index.jsx
+++ src/components/notelist/card/index.jsx
@@ -3,8 +3,10 @@ import cx from 'classnames';
 import './index.less'
 
 const Card = (props) => {
+    const { color } = props
+
     return <div className='card shadow'>
-        <div className={cx('head', props.color)}>
+        <div className={cx('head', color)}>
             <span className="title">标题</span>
             <span className="time">12秒前</span>
         </div>
